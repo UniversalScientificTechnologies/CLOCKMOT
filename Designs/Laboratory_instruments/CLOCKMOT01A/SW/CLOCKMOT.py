64585d213453d5ebec2ca3a12ecda65ff1080018
kaklik <kaklik@f55caa52-0715-0410-8694-db4083128028>
2016-09-05 11:47:20 +0000

    uprava kodu pro ovladani pres I2C.

--- Designs/Laboratory_instruments/CLOCKMOT01A/SW/CLOCKMOT.py
+++ Designs/Laboratory_instruments/CLOCKMOT01A/SW/CLOCKMOT.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 import sys
 import time
-import spidev       # SPI binding
+import pymlab       # SPI binding
 import pylirc       # infrared receiver binding
 
 #### Script Arguments ###############################################
@@ -26,81 +26,127 @@ else:
     sys.exit(1)
 
 
+
+# Begin of Class Axis --------------------------------------------------
+
 class axis:
-    def __init__(self, SPI_handler, Direction, StepsPerUnit):
+    def __init__(self, SPI_CS, Direction, StepsPerUnit, MaxSpeed):
         ' One axis of robot '
-        self.spi = SPI_handler
+        self.CS = SPI_CS
         self.Dir = Direction
         self.SPU = StepsPerUnit
+        self.maxspeed = MaxSpeed
+
+        self.L6470_ABS_POS      =0x01
+        self.L6470_EL_POS       =0x02
+        self.L6470_MARK         =0x03
+        self.L6470_SPEED        =0x04
+        self.L6470_ACC          =0x05
+        self.L6470_DEC          =0x06
+        self.L6470_MAX_SPEED    =0x07
+        self.L6470_MIN_SPEED    =0x08
+        self.L6470_FS_SPD       =0x15
+        self.L6470_KVAL_HOLD    =0x09
+        self.L6470_KVAL_RUN     =0x0A
+        self.L6470_KVAL_ACC     =0x0B
+        self.L6470_KVAL_DEC     =0x0C
+        self.L6470_INT_SPEED    =0x0D
+        self.L6470_ST_SLP       =0x0E
+        self.L6470_FN_SLP_ACC   =0x0F
+        self.L6470_FN_SLP_DEC   =0x10
+        self.L6470_K_THERM      =0x11
+        self.L6470_ADC_OUT      =0x12
+        self.L6470_OCD_TH       =0x13
+        self.L6470_STALL_TH     =0x14
+        self.L6470_STEP_MODE    =0x16
+        self.L6470_ALARM_EN     =0x17
+        self.L6470_CONFIG       =0x18
+        self.L6470_STATUS       =0x19
+
         self.Reset()
         self.Initialize()
+        self.MaxSpeed(self.maxspeed)
 
     def Reset(self):
         'Reset the Axis'
-        self.spi.xfer([0xC0])      # reset
+        spi.SPI_write_byte(self.CS, 0xC0)      # reset
 
     def Initialize(self):
         'set default parameters for H-bridge '
-#        self.spi.xfer( 0x14)      # Stall Treshold setup
-#        self.spi.xfer( 0xFF)  
-#        self.spi.xfer( 0x13)      # Over Current Treshold setup 
-#        self.spi.xfer( 0xFF)  
-        self.spi.xfer([0x15])      # Full Step speed 
-        self.spi.xfer([0xFF])
-        self.spi.xfer([0xFF]) 
-        self.spi.xfer([0x05])      # ACC 
-        self.spi.xfer([0x00])
-        self.spi.xfer([0x10]) 
-        self.spi.xfer([0x06])      # DEC 
-        self.spi.xfer([0x00])
-        self.spi.xfer([0x10]) 
-        self.spi.xfer([0x0A])      # KVAL_RUN
-        self.spi.xfer([0x05])
-        self.spi.xfer([0x0B])      # KVAL_ACC
-        self.spi.xfer([0x05])
-        self.spi.xfer([0x0C])      # KVAL_DEC
-        self.spi.xfer([0x05])
-#        self.spi.xfer([0x18])      # CONFIG
-#        self.spi.xfer([0b00111010])
-#        self.spi.xfer([0b10001000])
+#        spi.SPI_write_byte(self.CS, 0x14)      # Stall Treshold setup
+#        spi.SPI_write_byte(self.CS, 0xFF)  
+#        spi.SPI_write_byte(self.CS, 0x13)      # Over Current Treshold setup 
+#        spi.SPI_write_byte(self.CS, 0xFF)  
+        spi.SPI_write_byte(self.CS, 0x15)      # Full Step speed 
+        spi.SPI_write_byte(self.CS, 0xFF)
+        spi.SPI_write_byte(self.CS, 0xFF) 
+        spi.SPI_write_byte(self.CS, 0x05)      # ACC 
+        spi.SPI_write_byte(self.CS, 0x00)
+        spi.SPI_write_byte(self.CS, 0x10) 
+        spi.SPI_write_byte(self.CS, 0x06)      # DEC 
+        spi.SPI_write_byte(self.CS, 0x00)
+        spi.SPI_write_byte(self.CS, 0x10) 
+        spi.SPI_write_byte(self.CS, self.L6470_KVAL_RUN)      # KVAL_RUN
+        spi.SPI_write_byte(self.CS, 0x58)
+        spi.SPI_write_byte(self.CS, self.L6470_KVAL_ACC)      # KVAL_ACC
+        spi.SPI_write_byte(self.CS, 0x58)
+        spi.SPI_write_byte(self.CS, self.L6470_KVAL_DEC)      # KVAL_DEC
+        spi.SPI_write_byte(self.CS, 0x58)
+#        spi.SPI_write_byte(self.CS, 0x18)      # CONFIG
+#        spi.SPI_write_byte(self.CS, 0b00111000)
+#        spi.SPI_write_byte(self.CS, 0b00000000)
+        self.MaxSpeed(self.maxspeed)
+
+    def setKVAL(self, hold = 0.5, run = 0.5, acc = 0.5, dec = 0.5):
+        """ The available range is from 0 to 0.996 x VS with a resolution of 0.004 x VS """
+
+    def setOverCurrentTH(self, hold = 0.5, run = 0.5, acc = 0.5, dec = 0.5):
+        """ The available range is from 375 mA to 6 A, in steps of 375 mA """
+
+
       
     def MaxSpeed(self, speed):
-        'Setup of maximum speed in steps/s'
+        'Setup of maximum speed in steps/s. The available range is from 15.25 to 15610 step/s with a resolution of 15.25 step/s.'
         speed_value = int(speed / 15.25)
-        if (speed_value == 0):
+        if (speed_value <= 0):
             speed_value = 1
-        print hex(speed_value)
+        elif (speed_value >= 1023):
+            speed_value = 1023
 
-        data = [(speed_value >> i & 0xff) for i in (16,8,0)]
-        self.spi.xfer([data[0]])       # Max Speed setup 
-        self.spi.xfer([data[1]])
-        self.spi.xfer([data[2]])
+        data = [(speed_value >> i & 0xff) for i in (8,0)]
+        print data
+        spi.SPI_write_byte(self.CS, self.L6470_MAX_SPEED)       # Max Speed setup 
+        spi.SPI_write_byte(self.CS, data[0])
+        spi.SPI_write_byte(self.CS, data[1])
         return (speed_value * 15.25)
 
     def ReleaseSW(self):
         ' Go away from Limit Switch '
         while self.ReadStatusBit(2) == 1:           # is Limit Switch ON ?
-            self.spi.xfer([0x92 | (~self.Dir & 1)])     # release SW 
-            while self.GetStatus()['BUSY']:
+            spi.SPI_write_byte(self.CS, 0x92 | (~self.Dir & 1))     # release SW 
+            while self.IsBusy():
                 pass
             self.MoveWait(10)           # move 10 units away
  
     def GoZero(self, speed):
         ' Go to Zero position '
         self.ReleaseSW()
-        self.spi.xfer([0x82 | (self.Dir & 1)])       # Go to Zero
-        self.spi.xfer([0x00])
-        self.spi.xfer([speed])  
-        while self.GetStatus()['BUSY']:
+
+        spi.SPI_write_byte(self.CS, 0x82 | (self.Dir & 1))       # Go to Zero
+        spi.SPI_write_byte(self.CS, 0x00)
+        spi.SPI_write_byte(self.CS, speed)  
+        while self.IsBusy():
             pass
         time.sleep(0.3)
         self.ReleaseSW()
 
     def GetStatus(self):
         #self.spi.xfer([0b11010000])  # Get status command from datasheet - does not work for uknown rasons
-        self.spi.xfer([0x39])       # Gotparam  command on status register
-        data = self.spi.readbytes(1)
-        data = data + self.spi.readbytes(1)
+        spi.SPI_write_byte(self.CS, 0x39)       # Gotparam  command on status register
+        spi.SPI_write_byte(self.CS, 0x00)
+        data = [spi.SPI_read_byte()]
+        spi.SPI_write_byte(self.CS, 0x00)
+        data = data + [spi.SPI_read_byte()]
 
         status = dict([('SCK_MOD',data[0] & 0x80 == 0x80),  #The SCK_MOD bit is an active high flag indicating that the device is working in Step-clock mode. In this case the step-clock signal should be provided through the STCK input pin. The DIR bit indicates the current motor direction
                     ('STEP_LOSS_B',data[0] & 0x40 == 0x40),
@@ -119,75 +165,102 @@ class axis:
                     ('HIZ',data[1] & 0x01 == 0x01)])
         return status
 
+    def GetACC(self):
+#        self.spi.xfer([0x29])       # Gotparam  command on status register
+        spi.SPI_write_byte(self.CS, self.L6470_ACC + 0x20)  # TODO check register read address seting         
+        spi.SPI_write_byte(self.CS, 0x00)
+        data = spi.SPI_read_byte()
+        spi.SPI_write_byte(self.CS, 0x00)
+        data = data + [spi.SPI_read_byte()]
+        print data                      # return speed in real units
+
+
     def Move(self, units):
         ' Move some distance units from current position '
         steps = units * self.SPU  # translate units to steps 
         if steps > 0:                                          # look for direction
-            self.spi.xfer([0x40 | (~self.Dir & 1)])       
+            spi.SPI_write_byte(self.CS, 0x40 | (~self.Dir & 1))       
         else:
-            self.spi.xfer([0x40 | (self.Dir & 1)]) 
+            spi.SPI_write_byte(self.CS, 0x40 | (self.Dir & 1)) 
         steps = int(abs(steps))     
-        self.spi.xfer([(steps >> 16) & 0xFF])
-        self.spi.xfer([(steps >> 8) & 0xFF])
-        self.spi.xfer([steps & 0xFF])
+        spi.SPI_write_byte(self.CS, (steps >> 16) & 0xFF)
+        spi.SPI_write_byte(self.CS, (steps >> 8) & 0xFF)
+        spi.SPI_write_byte(self.CS, steps & 0xFF)
 
     def Run(self, direction, speed):
         speed_value = int(speed / 0.015)
-        print speed_value
+        print hex(speed_value)
 
-        data = [0b01010000 + direction]
-        data = data +[(speed_value >> i & 0xff) for i in (16,8,0)]
-        self.spi.xfer([data[0]])       # Max Speed setup 
-        self.spi.xfer([data[1]])
-        self.spi.xfer([data[2]])  
-        self.spi.xfer([data[3]])
+        command = 0b01010000 + int(direction)  
+        data = [(speed_value >> i & 0xff) for i in (16,8,0)]
+        spi.SPI_write_byte(self.CS, command)       # Max Speed setup 
+        spi.SPI_write_byte(self.CS, data[0])
+        spi.SPI_write_byte(self.CS, data[1])  
+        spi.SPI_write_byte(self.CS, data[2])
         return (speed_value * 0.015)
 
     def MoveWait(self, units):
         ' Move some distance units from current position and wait for execution '
         self.Move(units)
         while self.GetStatus()['BUSY']:
-            pass
-            time.sleep(0.8)
+            time.sleep(0.1)
 
     def Float(self, hard = False):
         ' switch H-bridge to High impedance state '
         if (hard == False):
-            self.spi.xfer([0xA0])
+            spi.SPI_write_byte(self.CS, 0xA0)
         else:
-            self.spi.xfer([0xA8])
+            spi.SPI_write_byte(self.CS, 0xA8)
 
 
 # End Class axis --------------------------------------------------
+
 
 print "Clock motor control script started. \r\n"
 print "Requested speed is: %f steps/s" % SPEED
 
 pylirc.init("pylirc", "/home/odroid/conf")
 
+
+
+cfg = config.Config(
+    i2c = {
+        "port": 1,
+    },
+
+    bus = [
+        { 
+        "name":"spi", 
+        "type":"i2cspi", 
+        "address": 0x2e,
+        },
+    ],
+)
+
+
+cfg.initialize()
+
+print "Stepper motor control test started. \r\n"
+print "Max motor speed: %d " % SPEED
+print "Distance to run: %d " % DISTANCE
+
+spi = cfg.get_device("spi")
+
+spi.route()
+
+
 try:
     print "Configuring SPI.."
-    spi = spidev.SpiDev() # create a spi object
-    spi.open(0, 0) # open spi port 0, device (CS) 0 
-    spi.mode = 0b01
-    spi.lsbfirst = False
-    spi.bits_per_word = 8
-    spi.cshigh = False
-    spi.max_speed_hz = 100000
-    #spi.SPI_config(spi.I2CSPI_MSB_FIRST| spi.I2CSPI_MODE_CLK_IDLE_HIGH_DATA_EDGE_TRAILING| spi.I2CSPI_CLK_461kHz)
-    time.sleep(1)
+    spi.SPI_config(spi.I2CSPI_MSB_FIRST| spi.I2CSPI_MODE_CLK_IDLE_HIGH_DATA_EDGE_TRAILING| spi.I2CSPI_CLK_461kHz)
+    time.sleep(0.1)
 
     print "Configuring stepper motor.."
-    X = axis(spi, 0, 1)    # set Number of Steps per axis Unit and set Direction of Rotation
+    X = axis(spi.I2CSPI_SS0, 0, 1, MaxSpeed = SPEED)    # set Number of Steps per axis Unit and set Direction of Rotation
 #    maximum_speed = X.MaxSpeed(100.0)
     #X.GetStatus()
 
 #    print "Motor speed limit is: %f steps/s" % maximum_speed
     running = False
-
-    while True:
-        pass
-
 
     print "Waiting for IR command.."
     while True:                    # set maximal motor speed 
